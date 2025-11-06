7cc3d26c3b6c926446d66eea79e1f158d63358d9
Jonas Hahn <jonashahn1@gmx.net>
2025-11-06 01:59:21 +0100

    auto up  01:59:20  up   7:21,  2 users,  load average: 0.17, 0.29, 0.37

--- cli/dot-config/yazi/init.lua
+++ cli/dot-config/yazi/init.lua
@@ -1,18 +1,35 @@
 require("starship"):setup()
 
 require("gpg"):setup {
-  default_recipient = "jonashahn1@gmx.net",
-  delete_after = true,
+    default_recipient = "jonashahn1@gmx.net",
+    delete_after = true,
 }
+ 
+require("git"):setup()
+
+-- https://github.com/MasouShizuka/projects.yazi
+require("projects"):setup({
+    save = {
+        method = "lua",
+    },
+    last = {
+        update_after_save = true,
+        update_after_load = true,
+        load_after_start = true,
+    },
+    notify = {
+        enable = false,
+    },
+})
 
 function Linemode:line_info()
-	local time = math.floor(self._file.cha.mtime or 0)
-	if time == 0 then
-		time = ""
-	elseif os.date("%Y", time) == os.date("%Y") then
-		time = os.date("%b %d %H:%M", time)
-	else
-		time = os.date("%b %d  %Y", time)
-	end
-	return time
+    local time = math.floor(self._file.cha.mtime or 0)
+    if time == 0 then
+        time = ""
+    elseif os.date("%Y", time) == os.date("%Y") then
+        time = os.date("%b %d %H:%M", time)
+    else
+        time = os.date("%b %d  %Y", time)
+    end
+    return time
 end
